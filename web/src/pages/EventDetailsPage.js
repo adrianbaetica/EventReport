d34d3614f2df7f0e536912d55b70423c882053a5
Adi <adrian.baetica14@gmail.com>
2021-01-23 17:51:47 +0200

    small updates eventDetailsPage

--- web/src/pages/EventDetailsPage.js
+++ web/src/pages/EventDetailsPage.js
@@ -13,18 +13,20 @@ const EventDetailsPage = () => {
     <div className='event-details-page'>
       
       <CustomMap event ={ {id: {id}, lat: 44.43, long: 26.1}}/>
-      
-      <div className='event-details'>
-        <span><span className='detail-name'>Code:</span> <span className='detail-value'>{`earthquake`}</span></span>
-        <span><span className='detail-name'>Tag:</span> <span className='detail-value'>{`Disastrous`}</span></span>
-        <span><span className='detail-name'>Description:</span> <span className='detail-value'>{`Bla bla bla`}</span></span>
-        <span><span className='detail-name'>Time:</span> <span className='detail-value'>{`${new Date()}`}</span></span>
-        <span><span className='detail-name'>Photo:</span></span>
-      </div>
-      <span className='event-photo'>
-        <img src='https://static.scientificamerican.com/sciam/cache/file/7F5C87CE-C719-4650-B4E8AEF44415E806_source.jpg' alt='poza'/>
-      </span>
+      <div className='event-info'>
+        <div className='event-details'>
+          <span><span className='detail-name'>Code:</span> <span className='detail-value'>{`earthquake`}</span></span>
+          <span><span className='detail-name'>Tag:</span> <span className='detail-value'>{`Disastrous`}</span></span>
+          <span><span className='detail-name'>Description:</span> <span className='detail-value'>{`Bla bla bla`}</span></span>
+          <span><span className='detail-name'>Time:</span> <span className='detail-value'>{`${new Date()}`}</span></span>
+          <span><span className='detail-name'>Photo:</span></span>
+        </div>
 
+        <span className='event-photo'>
+          <img src='https://static.scientificamerican.com/sciam/cache/file/7F5C87CE-C719-4650-B4E8AEF44415E806_source.jpg' alt='poza'/>
+        </span>
+      
+      </div>
     </div>
   );
 };
